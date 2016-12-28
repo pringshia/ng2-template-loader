7d3d25a85e32217f77ccee89a4f7d9d24fa5870a
pringshia <pratik@ringshia.com>
2016-12-28 11:20:41 -0500

    feat(loader): add loader logic & tests

--- test/test.js
+++ test/test.js
@@ -2,7 +2,16 @@ var expect = require("chai").expect;
 var loader = require("../index");
 
 describe("ng2-template-loader", function() {
- it("should work!", function() {
-   expect(true).to.be.true;
+ it("should escape html correctly", function() {
+   var unescaped = "<a href='#'>Link</a>"
+   var escaped = "&lt;a href=&#39;#&#39;&gt;Link&lt;/a&gt;"
+
+   expect(loader(unescaped)).to.equal(escaped);
  });
+ it("should escape braces correctly", function() {
+   var unescaped = "var x = { a: { b: 2 } }";
+   var escaped = "var x = {{'{'}} a: {{'{'}} b: 2 {{'}'}} {{'}'}}";
+
+   expect(loader(unescaped)).to.equals(escaped);
+ })
 });
