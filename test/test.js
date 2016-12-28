3cd1e51f3a3a38de8ef1d50ae3affbe0a3c44d09
pringshia <pratik@ringshia.com>
2016-12-28 11:06:42 -0500

    chore: setup test environment

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+var loader = require("../index");
+
+describe("ng2-template-loader", function() {
+ it("should work!", function() {
+   expect(true).to.be.true;
+ });
+});
